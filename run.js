4565ff1aac0464854e85e57dab7d99900836d9ba
Shaleen Bengani <Shaleen@Shaleens-MacBook-Pro.local>
2018-05-11 16:01:41 +0400

    Add comments to run.js

--- run.js
+++ run.js
@@ -1,24 +1,19 @@
 Web3 = require('web3');
 solc = require('solc');
 fs   = require('fs');
+
+// Connect to blockchain
 web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+
+// Read and compile Voting.sol contract
 code = fs.readFileSync('Voting.sol').toString();
 compiledCode = solc.compile(code);
 
+// Parse contract
 abiDefinition = JSON.parse(compiledCode.contracts[':Voting'].interface);
 VotingContract = web3.eth.contract(abiDefinition);
 byteCode = compiledCode.contracts[':Voting'].bytecode;
-deployedContract = VotingContract.new(['P1','P2','P3','VP1','VP2','VP3'],{data: byteCode, from: web3.eth.accounts[0], gas: 4700000});
 
-deployedContract.address;
-contractInstance = VotingContract.at(deployedContract.address);
-
-// contractInstance.totalVotesFor.call('Rama')
-// contractInstance.voteForCandidate('Rama', {from: web3.eth.accounts[0]})
-// contractInstance.totalVotesFor.call('Rama').toLocaleString()
-//
-//
-//
-// {"constant":true,"inputs":[{"name":"","type":"address"}],"name":"hasVoted","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},
-//
-// {"constant":false,"inputs":[{"name":"addr","type":"address"}],"name":"checkIfVoted","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"}
+// Deploy contract
+deployedContract = VotingContract.new(['P1','P2','P3','VP1','VP2','VP3'], // Make changes here
+          {data: byteCode, from: web3.eth.accounts[0], gas: 4700000});
